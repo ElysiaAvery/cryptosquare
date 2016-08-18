5ef3617ec9b2e79b68edddc0d85e3c65219b0d50
Elysia Nason and Satchel Grant <satchel@>
2016-08-18 15:18:43 -0700

    add working javascript that returns a cryptosquare code in sets of five letter words

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,33 @@
 //BUSINESS LOGIC
 var Code = function(sentence){
+  var noPunctuation=[];
+  var punctuations = /[abcdefghijklmnopqrstuvwxyz0123456789]+/;
   sentence = sentence.toLowerCase();
-  var  //create an for loop comparing the message to a regex of a-z 0-9, that would push truths into an empty array.
-  return ;
+  for (var i=0; i<sentence.length; i++){
+    if (punctuations.test(sentence[i])){
+      noPunctuation.push(sentence[i]);
+    }
+  }
+  var columnCount = parseInt(Math.sqrt(noPunctuation.length));
+  var encryptedSentence = "";
+  for (var n=0; n<columnCount; n++){
+    for (var i = 0; i < noPunctuation.length; i+=columnCount){
+      if (noPunctuation[i+n])
+        encryptedSentence = encryptedSentence + noPunctuation[i+n];
+    }
+  }
+  var codex=[];
+  var chunkCount = Math.ceil(encryptedSentence.length/5);
+  for(var t=1; t <= chunkCount; t++){
+    if (t < chunkCount) {
+      codex.push(encryptedSentence.slice((t*5 - 5), t*5));
+    }
+    else {
+      codex.push(encryptedSentence.slice(t*5 - 5));
+    }
+  }
+  encryptedSentence = codex.join(" ");
+  return encryptedSentence;
 }
 
 //UI LOGIC
